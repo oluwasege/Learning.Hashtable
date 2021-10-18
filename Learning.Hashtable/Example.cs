1c9652bd302e2ab3516b97b64bfc0f7ca7d68866
Oluwasegun Akinpelu <82780450+oluwasege@users.noreply.github.com>
2021-10-18 10:18:05 +0100

    Add project files.

---
+++ Learning.Hashtable/Example.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning.Hashtable
+{
+    public class Example
+    {
+        
+        
+        public static void Dictionary()
+        {
+            //Create a new dictionary of strings, with string keys.
+            Dictionary<string, string> openWith = new();
+            //Add some elements to the dictionary. There are no duplicate keys, but some of the values are duplicates.
+            openWith.Add("txt", "notepad.exe");
+            openWith.Add("bmp", "paint.exe");
+            openWith.Add("dib", "paint.exe");
+            openWith.Add("rtf", "wordpad.exe");
+
+            //The Add method throws an exception if the new key is already in the dictionary.
+            try
+            {
+                openWith.Add("txt", "winword.exe");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("An element with Key = 'txt' already exists.");
+            }
+
+
+            //The Item property is another name for the indexer, so you can omit its name when accessing elements
+        }
+    }
+}
---
+++ Learning.Hashtable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Learning.Hashtable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
